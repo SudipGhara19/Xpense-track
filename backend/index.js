c956169ee7b45ce0b13c4126f60dc9afaabdd023
SudipGhara19 <sudipghara135@gmail.com>
2025-02-26 00:09:29 +0530

    added Front-end origin

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT
 
-const allowedOrigins = "http://localhost:5173"
+const allowedOrigins = "https://xpense-track-eosin.vercel.app"
 
 const corsOptions = {
     origin: function (origin, callback) {
